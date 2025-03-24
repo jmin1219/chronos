e1b75f8953005dae737e82784ee3901842087222
Jaymin <60022152+jmin1219@users.noreply.github.com>
2025-03-24 11:48:20 +0900

    Implement calendar view for Schedule page using fullcalendar library

---
+++ src/app/(features)/schedule/components/calendar-panel/CalendarPanel.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
+export default function CalendarPanel() {
+  return (
+    <div className="w-full h-full p-2 overflow-y-auto">
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="timeGridWeek"
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek",
+        }}
+        height="100%"
+        editable={false}
+        selectable={false}
+      />
+    </div>
+  );
+}
--- src/app/(features)/schedule/page.tsx
+++ src/app/(features)/schedule/page.tsx
@@ -1,3 +1,17 @@
+"use client";
+
+import CalendarPanel from "./components/calendar-panel/CalendarPanel";
+
 export default function SchedulePage() {
-  return <div>SchedulePage</div>;
+  return (
+    <div className="flex flex-col w-full h-full">
+      <div className="h-[60%] border-b">
+        <CalendarPanel />
+      </div>
+      <div className="flex flex-row h-[40%]">
+        <div className="w-1/2 border-r p-4">Insights Panel</div>
+        <div className="w-1/2 p-4">Task List Panel</div>
+      </div>
+    </div>
+  );
 }
