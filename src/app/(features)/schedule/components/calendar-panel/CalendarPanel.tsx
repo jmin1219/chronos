388b05925a57b0f3fabd376310baad70b0ed8e89
Jaymin <60022152+jmin1219@users.noreply.github.com>
2025-03-24 12:10:19 +0900

    Fetch deepwork sessions into Schedule page calendar

--- src/app/(features)/schedule/components/calendar-panel/CalendarPanel.tsx
+++ src/app/(features)/schedule/components/calendar-panel/CalendarPanel.tsx
@@ -4,8 +4,25 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import { useEnrichedSessionsQuery } from "@/hooks/useSessionsQuery";
+import { useMemo } from "react";
+import { EnrichedSessionType } from "@/lib/types/deepwork_sessions";
 
 export default function CalendarPanel() {
+  const { data: sessions = [] } = useEnrichedSessionsQuery();
+
+  const events = useMemo(() => {
+    return sessions.map((session: EnrichedSessionType) => ({
+      id: String(session.id),
+      title: session.taskTitle ?? "Untitled Task",
+      start: new Date(session.startTime),
+      end: new Date(session.endTime),
+      backgroundColor: session.projectColor ?? "#999",
+      display: "block",
+      opacity: 0.5,
+    }));
+  }, [sessions]);
+
   return (
     <div className="w-full h-full p-2 overflow-y-auto">
       <FullCalendar
@@ -16,7 +33,10 @@ export default function CalendarPanel() {
           center: "title",
           right: "dayGridMonth,timeGridWeek",
         }}
+        nowIndicator={true}
+        slotDuration="00:15:00"
         height="100%"
+        events={events}
         editable={false}
         selectable={false}
       />
